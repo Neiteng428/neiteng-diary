47173623a2613566eb03dd7069d607e4ce9cf273
Tamaki Naito <tnaito.0367@gmail.com>
2023-03-26 15:04:42 +0900

    コメントの修正

--- firebase.js
+++ firebase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Work only on window
+// Work only on browser
 if(typeof window !== 'undefined'){
   const analytics = getAnalytics(app);
 }
